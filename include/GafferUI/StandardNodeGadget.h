9550ce6326254c3187457f34e40ca1975451ed23
John Haddon <thehaddonyoof@gmail.com>
2014-04-28 17:25:31 +0100

    Fixed rebase.

--- include/GafferUI/StandardNodeGadget.h
+++ include/GafferUI/StandardNodeGadget.h
@@ -116,10 +116,6 @@ class StandardNodeGadget : public NodeGadget
 		LinearContainer *noduleContainer( Edge edge );
 		const LinearContainer *noduleContainer( Edge edge ) const;
 
-	private :
-
-		NodulePtr addNodule( Gaffer::PlugPtr plug );
-
 		IndividualContainer *contentsContainer();
 		const IndividualContainer *contentsContainer() const;
 
