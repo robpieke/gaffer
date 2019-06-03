8af1d9124d2f08e22c0e9882d701233444e9a41e
John Haddon <thehaddonyoof@gmail.com>
2019-06-03 15:27:34 +0100

    muckingAboutWithAzureQueries

--- config/azure/pullRequestComment.py
+++ config/azure/pullRequestComment.py
@@ -1,25 +1,51 @@
 import github
+import azure.devops
+
+print azure.devops.__file__
+
+from azure.devops.connection import Connection
+from msrest.authentication import BasicAuthentication
 
 import argparse
 import os
 
-parser = argparse.ArgumentParser()
+credentials = BasicAuthentication( "", os.environ["AZURE_RELEASE_TOKEN"] )
+connection = Connection( base_url='https://dev.azure.com/thehaddonyoof', creds=credentials )
 
-parser.add_argument( "--organisation", required = True )
-parser.add_argument( "--repo", required = True )
-parser.add_argument( "--pr", required = True, type = int )
-parser.add_argument( "--comment", required = True )
+core_client = connection.clients.get_core_client()
 
-args = parser.parse_args()
+client = connection.clients_v5_1.get_build_client()
 
-g = github.Github( os.environ["GITHUB_RELEASE_TOKEN"] )
-repo = g.get_repo( args.organisation + "/" + args.repo )
-pr = repo.get_pull( args.pr )
-#pr.create_issue_comment( args.comment )
+print client
+
+artifact = client.get_artifact( "thehaddonyoof", 44, "testArtifact" )
+
+print artifact
+print artifact.resource
+
+# print dir( core_client )
+
+# print core_client.get_projects()[0].name
+
+# print core_client.get_project( "gaffer" )
+
+# parser = argparse.ArgumentParser()
+
+# parser.add_argument( "--organisation", required = True )
+# parser.add_argument( "--repo", required = True )
+# parser.add_argument( "--pr", required = True, type = int )
+# parser.add_argument( "--comment", required = True )
+
+# args = parser.parse_args()
+
+# g = github.Github( os.environ["GITHUB_RELEASE_TOKEN"] )
+# repo = g.get_repo( args.organisation + "/" + args.repo )
+# #pr = repo.get_pull( args.pr )
+# #pr.create_issue_comment( args.comment )
 
 
-commit = repo.get_commit( "93df97f" )
-print commit
+# commit = repo.get_commit( "e5f3b8f" )
+# print commit
 
-commit.create_status( "success", "http://gafferhq.org", "Download available", "Linux Build" )
-commit.create_status( "success", "http://gafferhq.org", "Download available", "MacOS Build" )
+# commit.create_status( "success", "http://gafferhq.org", "Download available", "Linux Build" )
+# commit.create_status( "success", "http://gafferhq.org", "Download available", "MacOS Build" )
